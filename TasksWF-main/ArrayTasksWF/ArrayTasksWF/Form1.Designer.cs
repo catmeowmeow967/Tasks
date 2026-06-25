da3e073b6d4a2ae319569be5b06c4f4b0116fccb
Голованов Егор <egorgol1017@gmail.com>
2026-06-25 21:31:24 +0300

    Add files via upload

---
+++ TasksWF-main/ArrayTasksWF/ArrayTasksWF/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace ArrayTasksWF
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.Button btnTask1;
+        private System.Windows.Forms.Button btnTask2;
+        private System.Windows.Forms.Button btnTask3;
+        private System.Windows.Forms.Button btnTask4;
+        private System.Windows.Forms.Button btnTask5;
+        private System.Windows.Forms.Button btnTask6;
+        private System.Windows.Forms.RichTextBox output;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.btnTask1 = new System.Windows.Forms.Button();
+            this.btnTask2 = new System.Windows.Forms.Button();
+            this.btnTask3 = new System.Windows.Forms.Button();
+            this.btnTask4 = new System.Windows.Forms.Button();
+            this.btnTask5 = new System.Windows.Forms.Button();
+            this.btnTask6 = new System.Windows.Forms.Button();
+            this.output = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnTask1
+            // 
+            this.btnTask1.Location = new System.Drawing.Point(12, 12);
+            this.btnTask1.Name = "btnTask1";
+            this.btnTask1.Size = new System.Drawing.Size(120, 40);
+            this.btnTask1.TabIndex = 0;
+            this.btnTask1.Text = "Задача 1";
+            this.btnTask1.UseVisualStyleBackColor = true;
+            this.btnTask1.Click += new System.EventHandler(this.btnTask1_Click);
+            // 
+            // btnTask2
+            // 
+            this.btnTask2.Location = new System.Drawing.Point(150, 12);
+            this.btnTask2.Name = "btnTask2";
+            this.btnTask2.Size = new System.Drawing.Size(120, 40);
+            this.btnTask2.TabIndex = 1;
+            this.btnTask2.Text = "Задача 2";
+            this.btnTask2.UseVisualStyleBackColor = true;
+            this.btnTask2.Click += new System.EventHandler(this.btnTask2_Click);
+            // 
+            // btnTask3
+            // 
+            this.btnTask3.Location = new System.Drawing.Point(290, 12);
+            this.btnTask3.Name = "btnTask3";
+            this.btnTask3.Size = new System.Drawing.Size(120, 40);
+            this.btnTask3.TabIndex = 2;
+            this.btnTask3.Text = "Задача 3";
+            this.btnTask3.UseVisualStyleBackColor = true;
+            this.btnTask3.Click += new System.EventHandler(this.btnTask3_Click);
+            // 
+            // btnTask4
+            // 
+            this.btnTask4.Location = new System.Drawing.Point(430, 12);
+            this.btnTask4.Name = "btnTask4";
+            this.btnTask4.Size = new System.Drawing.Size(120, 40);
+            this.btnTask4.TabIndex = 3;
+            this.btnTask4.Text = "Задача 4";
+            this.btnTask4.UseVisualStyleBackColor = true;
+            this.btnTask4.Click += new System.EventHandler(this.btnTask4_Click);
+            // 
+            // btnTask5
+            // 
+            this.btnTask5.Location = new System.Drawing.Point(570, 12);
+            this.btnTask5.Name = "btnTask5";
+            this.btnTask5.Size = new System.Drawing.Size(120, 40);
+            this.btnTask5.TabIndex = 4;
+            this.btnTask5.Text = "Задача 5";
+            this.btnTask5.UseVisualStyleBackColor = true;
+            this.btnTask5.Click += new System.EventHandler(this.btnTask5_Click);
+            // 
+            // btnTask6
+            // 
+            this.btnTask6.Location = new System.Drawing.Point(710, 12);
+            this.btnTask6.Name = "btnTask6";
+            this.btnTask6.Size = new System.Drawing.Size(120, 40);
+            this.btnTask6.TabIndex = 5;
+            this.btnTask6.Text = "Задача 6";
+            this.btnTask6.UseVisualStyleBackColor = true;
+            this.btnTask6.Click += new System.EventHandler(this.btnTask6_Click);
+            // 
+            // output
+            // 
+            this.output.Location = new System.Drawing.Point(12, 70);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(818, 368);
+            this.output.TabIndex = 6;
+            this.output.Text = "";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(842, 450);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.btnTask6);
+            this.Controls.Add(this.btnTask5);
+            this.Controls.Add(this.btnTask4);
+            this.Controls.Add(this.btnTask3);
+            this.Controls.Add(this.btnTask2);
+            this.Controls.Add(this.btnTask1);
+            this.Name = "Form1";
+            this.Text = "Массивы - 6 задач";
+            this.ResumeLayout(false);
+        }
+    }
+}
---
+++ TasksWF-main/ArrayTasksWF/ArrayTasksWF/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ArrayTasksWF
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+      
+        private void btnTask1_Click(object sender, EventArgs e)
+        {
+            output.Clear();
+
+            int[] arr = new int[10];
+            int sum = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                arr[i] = int.Parse(InputBox($"Введите элемент {i + 1}:"));
+                sum += arr[i];
+            }
+
+            output.AppendText("Сумма = " + sum);
+        }
+
+     
+        private void btnTask2_Click(object sender, EventArgs e)
+        {
+            output.Clear();
+
+            int N = int.Parse(InputBox("Введите размер массива:"));
+            int[] arr = new int[N];
+
+            for (int i = 0; i < N; i++)
+                arr[i] = rnd.Next(1, 101);
+
+            int max = arr[0], min = arr[0];
+            int maxIndex = 0, minIndex = 0;
+
+            for (int i = 1; i < N; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                    maxIndex = i;
+                }
+
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                    minIndex = i;
+                }
+            }
+
+            output.AppendText("Массив: " + string.Join(", ", arr) + Environment.NewLine);
+            output.AppendText($"Max = {max} (индекс {maxIndex})" + Environment.NewLine);
+            output.AppendText($"Min = {min} (индекс {minIndex})");
+        }
+
+       
+        private void btnTask3_Click(object sender, EventArgs e)
+        {
+            output.Clear();
+
+            int[] arr = new int[15];
+
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = rnd.Next(10, 51);
+
+            int even = 0, odd = 0;
+
+            foreach (int num in arr)
+            {
+                if (num % 2 == 0)
+                    even++;
+                else
+                    odd++;
+            }
+
+            output.AppendText("Массив: " + string.Join(", ", arr) + Environment.NewLine);
+            output.AppendText($"Четных: {even}" + Environment.NewLine);
+            output.AppendText($"Нечетных: {odd}");
+        }
+
+     
+        private void btnTask4_Click(object sender, EventArgs e)
+        {
+            output.Clear();
+
+            int[] arr = new int[5];
+
+            for (int i = 0; i < 5; i++)
+                arr[i] = int.Parse(InputBox($"Введите число {i + 1}:"));
+
+            int[] reversed = new int[5];
+
+            for (int i = 0; i < 5; i++)
+                reversed[i] = arr[4 - i];
+
+            output.AppendText("Исходный: " + string.Join(", ", arr) + Environment.NewLine);
+            output.AppendText("Реверс: " + string.Join(", ", reversed));
+        }
+
+     
+        private void btnTask5_Click(object sender, EventArgs e)
+        {
+            output.Clear();
+
+            int[] arr = { 1, 2, 3, 4, 5 };
+
+            int last = arr[arr.Length - 1];
+
+            for (int i = arr.Length - 1; i > 0; i--)
+                arr[i] = arr[i - 1];
+
+            arr[0] = last;
+
+            output.AppendText("После сдвига: " + string.Join(", ", arr));
+        }
+
+     
+        private void btnTask6_Click(object sender, EventArgs e)
+        {
+            output.Clear();
+
+            int[] arr = new int[10];
+
+            for (int i = 0; i < 10; i++)
+                arr[i] = int.Parse(InputBox($"Введите число {i + 1}:"));
+
+            bool[] counted = new bool[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (counted[i]) continue;
+
+                int count = 1;
+
+                for (int j = i + 1; j < 10; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        count++;
+                        counted[j] = true;
+                    }
+                }
+
+                output.AppendText($"Число {arr[i]} встречается {count} раз(а)" + Environment.NewLine);
+            }
+        }
+
+    
+        private string InputBox(string text)
+        {
+            Form form = new Form();
+            Label label = new Label();
+            TextBox textBox = new TextBox();
+            Button buttonOk = new Button();
+
+            form.Text = text;
+            label.Text = text;
+            buttonOk.Text = "OK";
+
+            label.SetBounds(10, 10, 300, 20);
+            textBox.SetBounds(10, 40, 300, 20);
+            buttonOk.SetBounds(120, 70, 75, 23);
+
+            buttonOk.DialogResult = DialogResult.OK;
+
+            form.Controls.Add(label);
+            form.Controls.Add(textBox);
+            form.Controls.Add(buttonOk);
+            form.AcceptButton = buttonOk;
+            form.ClientSize = new System.Drawing.Size(320, 110);
+
+            return form.ShowDialog() == DialogResult.OK ? textBox.Text : "";
+        }
+    }
+}
